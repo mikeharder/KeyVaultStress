06c20b41ac8a894edd89811e903cc045c6edcdb8
Mike Harder <mharder@microsoft.com>
2019-09-06 19:13:04 +0000

    Pass NODE_TLS_REJECT_UNAUTHORIZED env var

--- js/run.sh
+++ js/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --network host -e AZURE_TENANT_ID -e AZURE_CLIENT_ID -e AZURE_CLIENT_SECRET -e KEY_VAULT_URL keyvaultstress/js "$@"
+docker run -it --rm --network host -e AZURE_TENANT_ID -e AZURE_CLIENT_ID -e AZURE_CLIENT_SECRET -e KEY_VAULT_URL -e NODE_TLS_REJECT_UNAUTHORIZED keyvaultstress/js "$@"
